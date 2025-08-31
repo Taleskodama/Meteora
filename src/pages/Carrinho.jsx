c321423d187ed50a2655ebe2d55794cea0af5ca0
Tales <taleskodama@gmail.com>
2025-08-31 13:17:00 -0300

    refatoração utilizando useMemo e useReducer

--- src/pages/Carrinho.jsx
+++ src/pages/Carrinho.jsx
@@ -4,15 +4,10 @@ import Titulo from "@/components/Titulo";
 import Sumario from "@/components/Sumario";
 import ListaProdutosCarrinho from "@/components/ListaProdutosCarrinho";
 import BannerCarrinho from "@/components/BannerCarrinho";
+import { useCarrinhoContext } from "../hooks/useCarrinhoContext";
 
-const Carrinho = ({
-  carrinho,
-  adicionarProduto,
-  removerProduto,
-  removerProdutoCarrinho,
-  valorTotalCarrinho,
-  quantidadeProdutos,
-}) => {
+const Carrinho = () => {
+  const { carrinho } = useCarrinhoContext();
   return (
     <>
       <BarraNavegacao />
@@ -27,7 +22,7 @@ const Carrinho = ({
               <Titulo className="text-center fw-bold mb-3 text-md-start">
                 Detalhes da compra
               </Titulo>
-              <ListaProdutosCarrinho />
+              <ListaProdutosCarrinho carrinho={carrinho} />
             </div>
             <Sumario />
           </div>
